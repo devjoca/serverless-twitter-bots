e56d085637a3fa43b5d16c9a90fd1fbea16a2987
Joca <jpereyraleon@gmail.com>
2020-08-31 22:27:50 -0500

    modify and update songs

--- handler.py
+++ handler.py
@@ -8,19 +8,19 @@ import os
 def main(data, context):
     with open('./songs.json') as f:
         data = json.load(f)
+        lyrics = random.choice(data['songs'])['lyrics']
+        while (lyrics is None):
+            lyrics = random.choice(data['songs'])['lyrics']
+        songWithoutBrackets = re.sub(r'\[.*?\]', '', lyrics).split('\n')
+        linesOfSong = list(filter(lambda x: x != "", songWithoutBrackets))
 
-    lyrics = random.choice(data['songs'])['lyrics']
-    songWithoutBrackets = re.sub(r'\[.*?\]', '', lyrics).split('\n')
-    lineToTweet = random.choice(
-        list(filter(lambda x: x != "", songWithoutBrackets)))
-
-    auth = tweepy.OAuthHandler(os.environ['CONSUMER_KEY'],
-                               os.environ['CONSUMER_SECRET'])
-    auth.set_access_token(os.environ['ACCESS_KEY'],
-                          os.environ['ACCESS_SECRET'])
-    api = tweepy.API(auth)
-    api.update_status(status=lineToTweet)
+        auth = tweepy.OAuthHandler(os.environ['CONSUMER_KEY'],
+                                   os.environ['CONSUMER_SECRET'])
+        auth.set_access_token(os.environ['ACCESS_KEY'],
+                              os.environ['ACCESS_SECRET'])
+        api = tweepy.API(auth)
+        api.update_status(status=random.choice(linesOfSong))
 
 
 if __name__ == "__main__":
-    main()
+    main(None, None)
--- store-lyrics.py
+++ store-lyrics.py
@@ -1,8 +1,9 @@
 import lyricsgenius
+import os
 
-genius = lyricsgenius.Genius("GENIUS_API_KEY")
+genius = lyricsgenius.Genius(os.environ['GENIUS_ACCESS_TOKEN'])
 
 artist = genius.search_artist("Valentín y los Volcanes",
                               sort="title",
                               artist_id=484381)
-artist.save_lyrics('songs.json')
+artist.save_lyrics('songs.json', overwrite=True)
